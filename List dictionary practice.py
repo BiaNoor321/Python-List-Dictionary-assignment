5d687484f50bb201b8881005c0444196916b2a7a
Bia Noor <bianoor77@gmail.com>
2025-05-08 18:29:45 +0500

    Add files via upload

---
+++ List dictionary practice.py
@@ -0,0 +1,17 @@
+# Lists
+Fruits = ["mango","orange","strawberry"]
+print("Fruit_List:",Fruits)
+print("Third_item:",Fruits[2])
+Fruits.append("watermelon")
+print("Updated_fruits_list:",Fruits)
+
+# Dictionaries
+Student = {
+    "Name" : "Bia_Noor",
+    "Age" : 14,
+    "Grade" : "8th"
+}
+print("Stdent_Info:",Student)
+print("Name:",Student["Name"])
+Student["School"] = "Govt_Girls_High_School_Fsd"
+print("Updated_Studend_Info:",Student)
